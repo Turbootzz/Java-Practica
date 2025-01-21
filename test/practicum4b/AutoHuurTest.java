7432fdfae6861f6757ff1a3734933ad517dbb78d
Thijs Herman <tc.herman@hotmail.com>
2025-01-21 12:34:07 +0100

    added more tests to practicum 4b

--- test/practicum4b/AutoHuurTest.java
+++ test/practicum4b/AutoHuurTest.java
@@ -3,6 +3,7 @@ package practicum4b;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class AutoHuurTest {
 
@@ -59,5 +60,81 @@ public class AutoHuurTest {
         ah1.setAantalDagen(4);
 
         assertEquals(180.0, ah1.totaalPrijs()); // 4 dagen * 50 - 10% korting = 180.0
+    }
+
+    @Test
+    public void testTotaalPrijsMetAlleenHuurderZonderDagen() {
+        AutoHuur ah1 = new AutoHuur();
+        Auto a1 = new Auto("Peugeot 207", 50.0);
+        ah1.setGehuurdeAuto(a1);
+        ah1.setAantalDagen(0);
+
+        double totaalPrijs = ah1.totaalPrijs();
+
+        assertEquals(0.0, totaalPrijs, 0.01);
+    }
+
+    @Test
+    public void testTotaalPrijsMetAutoEnHuurderVoorEenDag() {
+        AutoHuur ah1 = new AutoHuur();
+        Auto a1 = new Auto("Peugeot 207", 50.0);
+        Klant k1 = new Klant("Mijnheer de Vries");
+
+        k1.setKorting(10.0);
+        ah1.setGehuurdeAuto(a1);
+        ah1.setHuurder(k1);
+        ah1.setAantalDagen(1);
+
+        assertEquals(45.0, ah1.totaalPrijs());
+    }
+
+    @Test
+    public void testTotaalPrijsMetVeelDagenGehuurd() {
+        AutoHuur ah1 = new AutoHuur();
+        Auto a1 = new Auto("Peugeot 207", 50.0);
+        Klant k1 = new Klant("Mijnheer de Vries");
+
+        k1.setKorting(20.0);
+        ah1.setGehuurdeAuto(a1);
+        ah1.setHuurder(k1);
+        ah1.setAantalDagen(100);
+
+        assertEquals(4000.0, ah1.totaalPrijs()); // 100 dagen * 50.0 - 20% korting = 4000.0
+    }
+
+    @Test
+    public void testTotaalPrijsMetAutoEnHuurderZonderKorting() {
+        AutoHuur ah1 = new AutoHuur();
+        Auto a1 = new Auto("Peugeot 207", 50.0);
+        Klant k1 = new Klant("Mijnheer de Vries");
+
+        ah1.setGehuurdeAuto(a1);
+        ah1.setHuurder(k1);
+        ah1.setAantalDagen(2);
+
+        assertEquals(100.0, ah1.totaalPrijs());
+    }
+    @Test
+    public void testTotaalprijsZonderDagPrijsOpNul() {
+        AutoHuur ah1 = new AutoHuur();
+        Auto a1 = new Auto("Peugeot 207", 0.0);
+        Klant k1 = new Klant("Mijnheer de Vries");
+
+        ah1.setGehuurdeAuto(a1);
+        ah1.setHuurder(k1);
+        ah1.setAantalDagen(2);
+
+        assertEquals(0.0, ah1.totaalPrijs());
+    }
+
+    @Test
+    public void testTotaalPrijsZonderAutoMaarMetDagen() {
+        AutoHuur ah1 = new AutoHuur();
+        Klant k1 = new Klant("Mijnheer de Vries");
+
+        ah1.setHuurder(k1);
+        ah1.setAantalDagen(5);
+
+        assertEquals(0.0, ah1.totaalPrijs());
     }
 }
